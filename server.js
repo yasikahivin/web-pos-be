d52a171e8b62e42e3b2fd7f3b0f8339f2168599a
Yasika Hivin Gunathilaka <38648741+yasikahivin@users.noreply.github.com>
2023-08-24 18:37:07 +0530

    server implementation

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/webpos', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
+});
+
+// Define your routes
+app.use('/api/clients', require('./routes/clients'));
+// app.use('/api/products', require('./routes/products'));
+// app.use('/api/invoices', require('./routes/invoices'));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
