01f833a010524407689fcde5c578518d9c115350
Yasika Hivin Gunathilaka <38648741+yasikahivin@users.noreply.github.com>
2024-01-12 14:54:59 +0530

    Server modify

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); //An Object Data mapper for connect MongoDB to JavaScript
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -22,7 +22,21 @@ mongoose
     console.error("MongoDB connection error:", err);
   });
 
-// Define your routes
+/*
+  // No longer necessary after mongoose 6:
+
+  mongoose.set('useFindAndModify', false);
+
+  await mongoose.connect('mongodb://127.0.0.1:27017/test', {
+    useNewUrlParser: true, // <-- no longer necessary
+    useUnifiedTopology: true // <-- no longer necessary
+  });
+
+  Mongoose 6 always behaves as if useNewUrlParser, useUnifiedTopology, and useCreateIndex are true
+
+*/
+
+// Define routes
 app.use("/api/clients", require("./routes/clients"));
 app.use("/api/products", require("./routes/products"));
 app.use("/api/invoices", require("./routes/invoices"));
